9a11b1bec5f85676ac90e8b6b263c2c907e8a445
yamensaad <yamen.saa199@gmail.com>
2021-12-17 13:42:04 +0100

    Add project files.

---
+++ web_scrapping1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_scrapping1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
